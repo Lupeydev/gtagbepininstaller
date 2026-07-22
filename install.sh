fe147980673d06a08adb9284e9b79612868fbe64
Lupey <lupey.dev@proton.me>
2026-07-22 03:37:24 -0500

    Init Commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+gtagpath="/home/lupey/.local/share/Steam/steamapps/common/Gorilla Tag/"
+
+wget https://github.com/BepInEx/BepInEx/releases/download/v5.4.23.5/BepInEx_win_x64_5.4.23.5.zip
+
+unzip -d Bepinfolder BepInEx_win_x64_5.4.23.5.zip
+
+mv Bepinfolder/* "$gtagpath"
+
+rm -rf BepInEx_win_x64_5.4.23.5.zip
+rm -rf Bepinfolder
+
+echo "Do you want any mods"
+read choicemods
+
