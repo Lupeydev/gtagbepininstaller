b44c7d2e8a263d4fd6fde852dad77bc1832b7d3f
Lupey <lupey.dev@proton.me>
2026-07-29 00:39:47 -0500

    added user base

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-gtagpath="/home/lupey/.local/share/Steam/steamapps/common/Gorilla Tag/"
+#!/usr/bin/env bash
+
+gtagpath="$HOME/.local/share/Steam/steamapps/common/Gorilla Tag/"
 
 wget https://github.com/BepInEx/BepInEx/releases/download/v5.4.23.5/BepInEx_win_x64_5.4.23.5.zip
 
